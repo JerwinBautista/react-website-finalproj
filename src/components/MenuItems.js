3dcb9e16193335ce4cbabaaa1a672ebad76a4813
Jerwin Bautista <bautistajerwin18@gmail.com>
2023-12-03 22:55:18 +0800

    first commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,42 @@
+export const MenuItems = [
+    {
+        title : "Home",
+        url : "/",
+        cName :  "nav-links",
+        icon : "fa-solid fa-house"
+    },
+    {
+        title : "About",
+        url : "/about",
+        cName :  "nav-links",
+        icon : "fa-solid fa-address-card"
+    },
+    {
+        title : "Contact",
+        url : "/contact",
+        cName :  "nav-links",
+        icon : "fa-solid fa-phone"
+    },
+    {
+        title : "Products",
+        url : "/products",
+        cName :  "nav-links",
+        icon : "fa-solid fa-cart-shopping"
+    },
+    {
+        title : "Partners",
+        url : "/partners",
+        cName :  "nav-links",
+        icon : "fa-solid fa-handshake"
+    },
+    {
+        title : "Sign Up",
+        url : "/",
+        cName :  "nav-links-mobile",
+    },
+    {
+        title : "Log-In",
+        url : "/",
+        cName :  "nav-links-mobile",
+    },
+]
---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar"
+
+
+function Home() {
+    return (
+        <>
+        
+        <Navbar />
+        <h1>Welcome to Crafted</h1>
+        </>
+    )
+}
+
+
+
+export default Home;
